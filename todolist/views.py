0fe5050fef7ae98ff66474cbc2f8945d765e23e2
kei neme <keineme@NeskMPro.local>
2020-11-17 04:59:54 +0900

    test2

--- todolist/views.py
+++ todolist/views.py
@@ -29,7 +29,7 @@ def edit_task2(request, task_id):
             return redirect('home')
     else:
         form = TaskForm(instance=task)
-    return render(request, 'task.html', {'form': form}, {'task':task})
+    return render(request, 'edit.html', {'form': form}, {'task':task})
 
 def delete(request, task_id):
     item = Task.objects.get(id=task_id)
